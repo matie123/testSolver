355109f4e05ec07fbfb0706f767144aac3e8d450
matie123 <matie.rychlicki@gmail.com>
2026-04-12 12:01:07 +0200

    Added header description because why not

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+#TEST SOLVER BY MATIE123 09.04.26 - 12.04.26
+#This is test solver for funny exams that my teacher wants me to solve 10 each week
+#Assuming that each exam takes about 30 mins, times 10 this means about 5 hrs each week!
+#Who would want to spend so much time solving test's that i doubt anyone smarter than an ape
+#Would be able to pass? Yeah, no one. Anyway, for now I'll keep it for myself, as I am the only person
+#That could achieve AI-high score without teacher's suspicion. Maybe one day I'll add an option
+#To set desired score so some of the questions would be answered randomly purposely.
+#Oh and remember:
+#Власть рождает паразитов. Да здравствует анархия!
+
 import os
 import json
 import requests
 import cloudscraper
 import time
-
 from Tools.demo.spreadsheet import cellname
 from groq import Groq
 import random
